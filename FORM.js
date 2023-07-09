689e436dcc53bc86c9ec5c738c475d0dcb3b9934
Nitin Roy <72542409+roynitin710@users.noreply.github.com>
2023-07-09 13:10:26 +0530

    Update FORM.js

--- FORM.js
+++ FORM.js
@@ -1,1 +1,50 @@
+import React, { useState } from 'react';
 
+const StudentForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [grade, setGrade] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({ name, age, grade, email });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Age:
+        <input type="number" value={age} onChange={(e) => setAge(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Grade:
+        <input type="text" value={grade} onChange={(e) => setGrade(e.target.value)} />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+const App = () => {
+  const handleSubmit = (student) => {
+    // Do something with the submitted student data
+    console.log(student);
+  };
+
+  return (
+    <div>
+      <h1>Student Form</h1>
+      <StudentForm onSubmit={handleSubmit} />
+    </div>
+  );
+};
+
+export default App;
